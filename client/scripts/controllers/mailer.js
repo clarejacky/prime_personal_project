268d7017d09e3f40e9d067785d37c3307aaab76a
clarejacky <clare.jacky@gmail.com>
2015-06-04 13:48:25 -0500

    styling

--- client/scripts/controllers/mailer.js
+++ client/scripts/controllers/mailer.js
@@ -1,10 +1,15 @@
-app.controller('sendMessage', ["$scope", "$http", function($scope, $http){
+app.controller('sendMessage', ["$scope", "$http", '$location', function($scope, $http, $location){
     $scope.mail = {};
+
+    $scope.go = function ( path ) {
+        $location.path( path );
+    };
 
     $scope.mailer = function(mailContent) {
         $http.post('/mail/sendMail', mailContent).
             success(function(data) {
                 console.log(data);
+                $scope.go('/submission');
             });
     }
 
